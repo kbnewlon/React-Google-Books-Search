d2a03808ab116ace595536a2725bac56bd69f8b2
Kayla Newlon <kbnewlon@hotmail.com>
2020-12-16 22:19:25 -0800

    added API key

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -1,4 +1,8 @@
 import axios from "axios";
+const BASEURL = "https://www.googleapis.com/books/v1/volumes?q=";
+const BOOKURL = "https://www.googleapis.com/books/v1/volumes/";
+const KEY = "&key=";
+const APIKEY = "AIzaSyDmDHr4-clbDIhBsoLXM3t5TZfYdT0OFFo"
 
 export default {
   // Gets all books
@@ -16,5 +20,15 @@ export default {
   // Saves a book to the database
   saveBook: function(bookData) {
     return axios.post("/api/books", bookData);
+  }, 
+  //Searches google books
+  search: function (query) {
+    return axios.get(BASEURL + query + KEY + APIKEY)
+  },
+  //returns the searched book
+  singleBook: function (query){
+    return axios.get(BOOKURL + query);
   }
 };
+
+
