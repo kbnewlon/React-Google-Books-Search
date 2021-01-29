a976442c5d8d568aad4fd7175017507f4ad29406
Kayla Newlon <kbnewlon@hotmail.com>
2021-01-28 18:14:08 -0800

    set up connection to mongoDB

--- server.js
+++ server.js
@@ -1,13 +1,19 @@
 const express = require("express");
-
 const mongoose = require("mongoose");
+
 const routes = require("./routes");
 const app = express();
 const PORT = process.env.PORT || 3001;
+const bodyParser = require('body-parser')
+require('dotenv').config()
+
+//Bodyparser Middleware
+app.use(bodyParser.json());
 
 // Define middleware here
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
@@ -17,8 +23,15 @@ app.use(routes);
 
 // Connect to the Mongo DB
 mongoose.connect(
-  process.env.MONGODB_URI || "mongodb://localhost/googlebooks"
-);
+  process.env.MONGODB_URI || 'mongodb://localhost/googlebooks',
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+  }
+).then(() => console.log('MongoDB Connected...'))
+.catch(err => console.log(err))
 
 // Start the API server
 app.listen(PORT, function() {
